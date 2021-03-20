fde5515b8874df46eaa0654808c733b2387eb131
Angelica <Angelica137@users.noreply.github.com>
2021-03-20 20:51:26 +0000

    test guess prompt

--- WrappedUp.java
+++ WrappedUp.java
@@ -1,8 +1,9 @@
+import java.util.Scanner;
 
 /**
  * Favourite class to test a string object and it's methods.
  * 
- * @version (a version number or a date)
+ * @version 20 Mar 2021
  */
 
 public class WrappedUp {
@@ -11,13 +12,15 @@ public class WrappedUp {
 		// Create a string object with your favourite animal, car ..whatever
 		// e.g String favMotorBikeManufacturer = "bmw"
 		String myFave = "water";
-		System.out.println(myFave);
+
 		/*
 		 * Now prompt for a guess - using the Scanner class Using String methods, you
 		 * should give the length and first letter of your favourite thing in the
 		 * prompt.
 		 */
-
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Guess what my favourite thing is. 5 characters, starts with w.");
+		System.out.println(sc);
 		// Now test if the guess was correct - ignore case
 
 	}// end guessing
